7097745c15203417e24e6824a757f30cfe4eb7df
Murtaza Zaidi <murtazazaidi@outlook.com>
2018-01-02 19:43:21 +0500

    Babelify the code for increased support.

--- index.test.js
+++ index.test.js
@@ -1,119 +1,71 @@
-import LocalCache from './index';
+'use strict';
 
-const mockData = [{
+var _index = require('./lib/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var mockData = [{
   name: 'localCache',
   mockId: 1
-},{
+}, {
   name: 'local-Cache',
   mockId: 2
-}
-];
-const groups = ['a', 'b'];
-const keys = [1,2];
+}];
+var groups = ['a', 'b'];
+var keys = [1, 2];
 
-test('blank add with no error', () => {
-  expect(LocalCache.setData()).toBe(undefined);
+test('blank add with no error', function () {
+  expect(_index2.default.setData()).toBe(undefined);
 });
 
-test('blank fetch with no error', () => {
-  expect(LocalCache.getData()).toBe(null);
+test('blank fetch with no error', function () {
+  expect(_index2.default.getData()).toBe(null);
 });
 
-test('blank clear with no error', () => {
-  expect(LocalCache.clearData()).toBe(undefined);
-  expect(LocalCache.clearGroup()).toBe(undefined);
-  expect(LocalCache.clearAllCache()).toBe(undefined);
+test('blank clear with no error', function () {
+  expect(_index2.default.clearData()).toBe(undefined);
+  expect(_index2.default.clearGroup()).toBe(undefined);
+  expect(_index2.default.clearAllCache()).toBe(undefined);
 });
 
-test('set and get data', () => {
-  LocalCache.setData(
-    keys[0],
-    mockData[0],
-    300000,
-    groups[0]
-  );
-  expect(LocalCache.getData(
-    keys[0],
-    groups[0]
-  )).toBe(mockData[0]);
+test('set and get data', function () {
+  _index2.default.setData(keys[0], mockData[0], 300000, groups[0]);
+  expect(_index2.default.getData(keys[0], groups[0])).toBe(mockData[0]);
 });
 
-test('clear data', () => {
-  LocalCache.clearData(
-    keys[0],
-    groups[0]
-  );
-  expect(LocalCache.getData(
-    keys[0],
-    groups[0]
-  )).toBe(null);
+test('clear data', function () {
+  _index2.default.clearData(keys[0], groups[0]);
+  expect(_index2.default.getData(keys[0], groups[0])).toBe(null);
 });
 
-test('Add data and clear group', () => {
-  LocalCache.setData(
-    keys[0],
-    mockData[0],
-    300000,
-    groups[0]
-  );
-  LocalCache.setData(
-    keys[1],
-    mockData[1],
-    300000,
-    groups[1]
-  );
-  LocalCache.clearGroup(
-    groups[0],
-  );
-  expect(LocalCache.getData(
-    keys[0],
-    groups[0]
-  )).toBe(null);
-  expect(LocalCache.getData(
-    keys[1],
-    groups[1]
-  )).toBe(mockData[1]);
+test('Add data and clear group', function () {
+  _index2.default.setData(keys[0], mockData[0], 300000, groups[0]);
+  _index2.default.setData(keys[1], mockData[1], 300000, groups[1]);
+  _index2.default.clearGroup(groups[0]);
+  expect(_index2.default.getData(keys[0], groups[0])).toBe(null);
+  expect(_index2.default.getData(keys[1], groups[1])).toBe(mockData[1]);
 });
 
-test('remove data from array false', () => {
-  LocalCache.clearData(
-    keys[0],
-    groups[1]
-  );
-  expect(LocalCache.getData(
-    keys[1],
-    groups[1]
-  )).toBe(mockData[1]);
+test('remove data from array false', function () {
+  _index2.default.clearData(keys[0], groups[1]);
+  expect(_index2.default.getData(keys[1], groups[1])).toBe(mockData[1]);
 });
 
-test('clear All data', () => {
-  LocalCache.clearAllCache();
-  expect(LocalCache.getData(
-    keys[1],
-    groups[1]
-  )).toBe(null);
+test('clear All data', function () {
+  _index2.default.clearAllCache();
+  expect(_index2.default.getData(keys[1], groups[1])).toBe(null);
 });
 
 jest.useFakeTimers();
-test('check cache timeout clear', () => {
-  LocalCache.setData(
-    keys[0],
-    mockData[0],
-    1000,
-    groups[0]
-  );
+test('check cache timeout clear', function () {
+  _index2.default.setData(keys[0], mockData[0], 1000, groups[0]);
   jest.runTimersToTime(1010);
-  expect(LocalCache.getData(
-    keys[0],
-    groups[0]
-  )).toBe(null);
+  expect(_index2.default.getData(keys[0], groups[0])).toBe(null);
 });
 
-test('clear All data when already empty', () => {
-  LocalCache.clearAllCache();
-  expect(LocalCache.getData(
-    keys[1],
-    groups[1]
-  )).toBe(null);
+test('clear All data when already empty', function () {
+  _index2.default.clearAllCache();
+  expect(_index2.default.getData(keys[1], groups[1])).toBe(null);
 });
-
